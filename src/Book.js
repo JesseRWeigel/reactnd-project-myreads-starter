8336920160c862e8c5cbe924a35072a06d659038
Jesse Weigel <jesseweigel@gmail.com>
2017-07-07 23:36:00 -0400

    Refactor: Create book component and populate with data from BooksAPI

---
+++ src/Book.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+
+const Book = (props) => (
+  <li key={props.bookID}>
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.image}")` }}></div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{props.title}</div>
+      <div className="book-authors">{props.authors.map(author => <span style={{display: 'block'}}>{author}</span>)}</div>
+    </div>
+  </li>
+)
+
+export default Book
